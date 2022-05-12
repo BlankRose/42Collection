ea475c46913e5f337e235d1d19b5144df7c2f231
Rosie <collard.florian@yahoo.com>
2022-05-12 18:13:46 +0200

    Makefile Updates AGAIN

--- pipexx/pipex.h
+++ pipexx/pipex.h
@@ -3,29 +3,25 @@
 /*                                                        :::      ::::::::   */
 /*   pipex.h                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: cciobanu <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: flcollar <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/23 12:45:44 by cciobanu          #+#    #+#             */
-/*   Updated: 2022/04/23 12:45:58 by cciobanu         ###   ########.fr       */
+/*   Updated: 2022/05/12 18:04:47 by flcollar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PIPEX_H
 # define PIPEX_H
 
+# include "../libft/libft.h"
 # include <unistd.h>
 # include <stdio.h>
 # include <sys/wait.h>
 # include <fcntl.h>
 # include <stdlib.h>
 
-size_t	ft_strlen(const char *s);
 int		ft_wordcount(const char *str, char c);
-char	**ft_split(char const *str, char c);
-char	*ft_substr(char const *s, unsigned int start, size_t len);
-char	*ft_strjoin(char const *s1, char const *s2);
-char	*ft_strdup(const char *src);
 char	*ft_getbin(char *command, char **envp);
-int		ft_strncmp(const char *s1, const char *s2, size_t n);
 void	ft_freemem(char **arr);
+
 #endif
